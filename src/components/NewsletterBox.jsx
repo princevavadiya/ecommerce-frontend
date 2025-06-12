40f752ca1a84b295c4f145caa1bed62a82775e23
Prince <144765704+princevavadiya@users.noreply.github.com>
2025-06-12 14:17:49 +0530

    main mail

--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const NewsletterBox = () => {
 
 
   const [formData, setFormData] = useState({
 
-    email: "",
+    email: ""
 
   });
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
+    console.log("e")
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(setFormData)
   };
 
   const handleSubmit = async (e) => {
@@ -19,13 +22,13 @@ const NewsletterBox = () => {
 
     try {
       await axios.post("http://localhost:5000/send-mail", formData);
-      alert("Mail sent successfully!");
+      toast.success("Mail sent successfully!");
     } catch (error) {
       console.error(error);
-      alert("Failed to send mail");
+      toast.error("Failed to send mail");
     }
   };
- 
+
   return (
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscibe now & get 20% off</p>
@@ -33,9 +36,9 @@ const NewsletterBox = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos sequi earum corrupti, nemo in, provident ipsam magni .
       </p>
       <form onSubmit={handleSubmit} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 ' >
-        <input   onChange={handleChange}
-        className='w-full sm:flex-1 outline-none ' type="email" name="" id="" placeholder='Enter your email' required />
-        <button  className='bg-black text-white text-xs px-10 py-4 ' type='submit'>SUBSCRIBE</button>
+        <input onChange={handleChange} name="email"
+          className='w-full sm:flex-1 outline-none ' type="email" id="" placeholder='Enter your email' required />
+        <button className='bg-black text-white text-xs px-10 py-4 cursor-pointer ' type='submit' >SUBSCRIBE</button>
       </form>
     </div>
   )
