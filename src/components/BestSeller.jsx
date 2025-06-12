fd9c1c4e96dec789083310713916e664f2e54826
Prince <144765704+princevavadiya@users.noreply.github.com>
2025-06-12 11:43:13 +0530

    first commit

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+function BestSeller() {
+
+  const { products } = useContext(ShopContext)
+  const [bestSeller, setBestSeller] = useState([]);
+  useEffect(() => {
+    const bestProducts = products.filter((item) => (item.bestSeller));
+    setBestSeller(bestProducts.slice(0, 5))
+  }, [products])
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quis rem eos eum nam inventore velit fuga.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {
+          bestSeller.map((item, index) => (
+            <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+          ))
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default BestSeller
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import { useContext, useState } from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+import { Link, Navigate, NavLink } from "react-router"
+import { ShopContext } from '../context/ShopContext'
+import { toast } from 'react-toastify'
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false)
+  const { setShowSearch, getCartCount, token, setToken, setCartItems, navigate } = useContext(ShopContext)
+
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setCartItems({})
+    navigate("/login");
+    toast.success("Logout Successfully");
+
+  }
+
+  return (
+    <div className='flex justify-between items-center py-5 font-medium '>
+      <Link to="/">
+        <img src={assets.logo} className='w-36' alt="" />
+      </Link>
+      <ul className='hidden md:flex gap-5 text-sm text-gray-700 '>
+        <NavLink to="/" className="flex flex-col items-center gap-1 ">
+          <p>HOME</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 duration-300  ease-in hidden' />
+        </NavLink>
+
+        <NavLink to="/collection" className="flex flex-col items-center gap-1 ">
+          <p>COLLECTION</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+
+        <NavLink to="/about" className="flex flex-col items-center gap-1 ">
+          <p>ABOUT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+
+
+
+        <NavLink to="/contact" className="flex flex-col items-center gap-1 ">
+          <p>CONTACT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+
+        <Link to="https://ecommerce-admin-tau-six.vercel.app" className='flex flex-col items-center gap-1' target='_blank' >
+          <div className='border-b-2 border-gray-700 text-center text-blue-800  '>ADMIN</div>
+          {/* <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 ' /> */}
+
+        </Link>
+      </ul>
+
+      <div className='flex items-center gap-6'>
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <div className='group relative'>
+
+          <img onClick={() => token ? null : navigate('/login')} className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+          {
+            token ?
+              <div className='group-hover:block hidden absolute right-0 pt-4 '>
+                <div className='flex flex-col gap-2 w-36 px-5 py-3 bg-slate-100 text-gray-500 rounded '>
+                  <p className='cursor-pointer hover:text-black'>My Profile</p>
+                  <Link to="/orders">
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                  </Link>
+                  <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
+                </div>
+              </div> : null
+          }
+        </div>
+        <Link to="/cart" className='relative'>
+
+          <img src={assets.cart_icon} className='w-5 cursor-pointer min-w-5' alt="" />
+          <p className='absolute right-[-5px] bottom-[-5px] bg-black text-white rounded-full w-4 text-[8px]   text-center leading-4 aspect-square'>{getCartCount()}</p>
+        </Link>
+        <img onClick={() => setVisible(!visible)} src={assets.menu_icon} className='w-5 cursor-pointer md:hidden block' alt="" />
+
+      </div>
+      {/* Sidebar menu  */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white tranistion-all ease-in-out duration-300 ${visible ? 'w-full' : 'w-0'}`}>
+
+
+        <div className='flex flex-col text-gray-600 '>
+          <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer '>
+            <img className='h-4  rotate-180' src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6  text-center ' to="/">HOME</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 text-center ' to="/collection">COLLECTION</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 text-center' to="/about">ABOUT</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6  text-center' to="/contact">CONTACT</NavLink>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
